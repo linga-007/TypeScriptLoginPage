8a0af464f6954d82074a9f5156dbdc54056d74f6
Kishore-kumar-07 <bkdon54@gmail.com>
2024-01-14 11:40:02 +0530

    asfas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,21 @@
-import Image from "next/image";
+// import Image from "next/image";
+import axios from "axios";
+// import { useEffect } from "react";
 
 export default function Home() {
+  const handelRequest = async () => {
+    try {
+      const response = await axios.get("https://jsonplaceholder.typicode.com/todos");
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // useEffect(() => {
+  //   // This useEffect will run after the initial render
+  //   handelRequest();
+  // }, []); 
   return (
     <main>
       <div className="w-screen h-screen bg-zinc-50 flex justify-center items-center">
@@ -9,7 +24,7 @@ export default function Home() {
             efful
           </h2>
 
-          <h1 className="text-black t-[131px] l-[267px] text-Inter text-3xl font-medium flex pt-5 pb-8 justify-center items-center">
+          <h1 className="text-cyan-300 t-[131px] l-[267px] text-Inter text-3xl font-medium flex pt-5 pb-8 justify-center items-center">
             Log In
           </h1>
           <div className=" w-[425px] pt-8 ">
@@ -18,7 +33,7 @@ export default function Home() {
             </h2>
             <input
               placeholder="  admin@gmail.com"
-              className=" block w-[416px] bg-transparent h-[36px] border-2 border-[#8A8A8A] rounded-md"
+              className=" block w-[416px] bg-transparent h-[36px] border-2 border-[#8A8A8A] rounded-md text-black"
             ></input>
           </div>
           <div className=" w-[425px] pt-8 pb-1 ">
@@ -27,11 +42,11 @@ export default function Home() {
             </h2>
             <input
               placeholder="  *********"
-              className=" block w-[416px] bg-transparent h-[36px] border-2 border-[#8A8A8A] rounded-md"
+              className=" block w-[416px] bg-transparent h-[36px] border-2 border-[#8A8A8A] rounded-md text-black"
             ></input>
           </div>
           <div className=" w-[425px] pt-8 pb-1 ">
-            <button className=" block w-[416px] bg-[#49656D] h-[40px] rounded-md">
+            <button  className=" block w-[416px] bg-[#49656D] h-[40px] rounded-md">
               Log In
             </button>
           </div>
