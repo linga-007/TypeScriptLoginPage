29b3243c6c5efc9188a5d546ccfbf846af11df69
linga-007 <lingesh480500@gmail.com>
2024-01-15 07:10:01 +0530

    added a new page checkout address and designed the basic outline

---
+++ src/app/Checkout/page.tsx
@@ -0,0 +1,17 @@
+export default function Checkout(){
+  return (
+    <div className="w-screen h-screen bg-zinc-50 flex justify-center items-center">
+        <div className="h-[664px] w-[1024px] t-[180px] b-[407px]   flex absolute ">
+          <div className="w-[684px] rounded-tl-md rounded-bl-md bg-black flex justify-center" >
+            <h2 className="text-white">hello</h2>
+            
+          </div>
+          <div className="bg-slate-500 rounded-tr-md rounded-br-md w-[385px] flex justify-center ">
+            <h2 className="text-black">world</h2>
+           
+          </div>
+            
+        </div>
+    </div>
+  )
+}
