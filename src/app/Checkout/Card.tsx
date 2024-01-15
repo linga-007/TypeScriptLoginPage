d1d30dc46fbe5d64c17590551670237f8173eee6
linga-007 <lingesh480500@gmail.com>
2024-01-15 22:45:49 +0530

    worked on rhs and created div and added styling to it

---
+++ src/app/Checkout/Card.tsx
@@ -0,0 +1,9 @@
+
+
+const Card = () => {
+  return (
+    <div className="text-black h-[85px]">Card</div>
+  )
+}
+
+export default Card
--- src/app/Checkout/page.tsx
+++ src/app/Checkout/page.tsx
@@ -1,17 +1,23 @@
-export default function Checkout(){
+import Card from "./Card";
+
+export default function Checkout() {
   return (
     <div className="w-screen h-screen bg-zinc-50 flex justify-center items-center">
-        <div className="h-[664px] w-[1024px] t-[180px] b-[407px]   flex absolute ">
-          <div className="w-[684px] rounded-tl-md rounded-bl-md bg-black flex justify-center" >
-            <h2 className="text-white">hello</h2>
-            
-          </div>
-          <div className="bg-slate-500 rounded-tr-md rounded-br-md w-[385px] flex justify-center ">
-            <h2 className="text-black">world</h2>
-           
-          </div>
-            
+      <div className="h-[664px] w-[1024px] t-[180px] b-[407px]   flex absolute ">
+        <div className="w-[684px] rounded-tl-md rounded-bl-md bg-[#FFFFFF] flex justify-center">
+          <h2 className="text-black">hello</h2>
         </div>
+        <div className="bg-black rounded-tr-md rounded-br-md w-[385px] flex justify-center relative ">
+          <h2 className="">X</h2>
+          <div className="h-[290px] w-[360px] mt-16 absolute bg-white  ">
+            <div className="h-[48px] w-[360px]  absolute bg-slate-500  ">
+              <h2 className="text-Inter pl-12 pt-2.5">Order Summary</h2>
+              <Card/>
+              {/* need to be changed */}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
